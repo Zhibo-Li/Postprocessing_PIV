a0356efc9a8ab3776fb4ecea34c3be63b77844b7
Zhibo-Li <zhibo.li.1994@gmail.com>
2023-04-03 16:37:58 +0200

    Change: data paths.
    The averaged flow fields are put into one folder to be loaded and plotted.
    The simulation data also changed their locations.

--- uPIV_post.m
+++ uPIV_post.m
@@ -5,14 +5,12 @@ set(0, 'defaulttextinterpreter','latex');
 set(groot, 'defaultAxesTickLabelInterpreter','latex');  
 set(groot, 'defaultLegendInterpreter','latex');
 
-selpath = uigetdir('G:\PhD, PMMH, ESPCI\Experimental Data (EXTRACTED)\');
+selpath = uigetdir('F:\Processing & Results\PIV & PTV\20220811-uPIV\20220811_Channel2022-07-26_FlowAngle45deg_Characterization\Avg_Std\');
 listing = dir(selpath);
 
 Names = {listing.name};  % the file/folder names
 XblankArea = xor(contains(Names,'Lens='), contains(Names,'BlankArea_')); % only select the PAs related files/folders.
-thefolder_ind = cell2mat({listing.isdir}); % only select the folders.
-PAsArea = and(XblankArea, thefolder_ind); % the PAs folders.
-PAsArea_ind = find(PAsArea==1); % the indices of the PAs folders.
+PAsArea_ind = find(XblankArea==1); % the indices of the PAs folders.
 
 magni = 0.067; % the magnification of the objective (um/pixel)
 
@@ -37,7 +35,7 @@ crossnum = length(theCrossPoints);
 % normalize the X by the calculation gap length (for V-profile along flow direction).
 theNormX = round([theX1(2, :), theX2(2, :)]/12);
 
-layersNUM = sum(PAsArea);
+layersNUM = sum(XblankArea);
 Z = zeros(1, layersNUM);  UUU_alongZ = zeros(1, layersNUM); VVV_alongZ = zeros(1, layersNUM);
 UUU_alongZ_err = zeros(1, layersNUM); VVV_alongZ_err = zeros(1, layersNUM); 
 for ii = 1:layersNUM
@@ -48,7 +46,7 @@ for ii = 1:layersNUM
 %     v = loadvec(fullfile(selpath,theONE,'\AddGeometricMask_01\PIV_MPd(2x48x48_75%ov)\*.vc7'));  % to calculate the ave_field
 %     ave_field = averf(v);
 %     [ave_field_AF, ave_field_STD, ave_field_RMS] = averf(v);
-    load(fullfile(selpath,theONE,'\AddGeometricMask_01\PIV_MPd(2x48x48_75%ov)\ave_field_STD_RMS.mat')); % load the ave_field
+    load(fullfile(selpath,theONE)); % load the ave_field
 %     save(fullfile(selpath,theONE,'\AddGeometricMask_01\PIV_MPd(2x48x48_75%ov)\ave_field_STD_RMS.mat'), "ave_field_AF", "ave_field_RMS", "ave_field_STD");
     ave_field_phy = ave_field_AF; % in physical units.
     ave_field_phy.vx = ave_field_AF.vx * magni / deltaT; % velocity in m/s.
@@ -94,90 +92,88 @@ for ii = 1:layersNUM
 
 end
 
-%%% Velocity profile along the z-direction (absolute value).
-% figure('color', 'w'); set(gcf, 'Position', [100 100 600 450]);
-% errorbar(Z, VVV_alongZ*1e6, VVV_alongZ_err*1e6,'LineStyle','none', 'Marker','*','MarkerSize', 5); hold on
-% % % the simulation results: 
-% data = readmatrix('D:\Dropbox\PROCESS remotely\Simulation\202208\uPIVexp_H52\Ux_Z-direction.csv');
-% Speed_Simu = data(1:10:end, 1); ZZ = data(1:10:end, 9);
-% plot(ZZ*1e5, Speed_Simu*1e7, 'r--.', 'LineWidth', 2); hold on
-% xlabel('$Z\ ({\mu}m)$','FontSize',22,'Interpreter', 'latex');
-% ylabel('$U_x\ ({\mu}m/s)$','FontSize',22,'Interpreter', 'latex');
-% % % title('Speed in the Middle Plane','FontSize',48);
-% set(gca,'FontSize',20)
-% legend('PIV','Simulation','location','best','FontSize',20)
-% xlim([-0.5 52.5])
-% % f=gcf;
-% % exportgraphics(f,'D:\Dropbox\Research\All Plottings\General plots\20220811_PIV-Simulation_PAsArea_H52_Ux-Z.png','Resolution',100)
-% 
-% 
-% %%% Velocity profile along the z-direction (normalized).
-% figure('color', 'w'); set(gcf, 'Position', [100 100 600 450]);
-% errorbar(Z/52-0.02, VVV_alongZ*1e6/147, VVV_alongZ_err*1e6/147,'LineStyle','none', 'Marker','*','MarkerSize', 5); hold on
-% % % the simulation results: 
-% data = readmatrix('D:\Dropbox\PROCESS remotely\Simulation\202208\uPIVexp_H52\Ux_Z-direction.csv');
-% Speed_Simu = data(1:10:end, 1); ZZ = data(1:10:end, 9);
-% plot(ZZ*1e5/52, Speed_Simu*1e7/157, 'r--.', 'LineWidth', 2); hold on
-% xlabel('$Z/H$','FontSize',22,'Interpreter', 'latex');
-% ylabel('$U_x/U_{peak}\ in\ blank\ area$','FontSize',22,'Interpreter', 'latex');
-% % % title('Speed in the Middle Plane','FontSize',48);
-% set(gca,'FontSize',20)
-% legend('PIV','Simulation','location','best','FontSize',20)
-% xlim([0 1])
-% % f=gcf;
-% % exportgraphics(f,'D:\Dropbox\Research\All Plottings\General plots\20220811_PIV-Simulation_PAsArea_H52_Ux-Z_normalized.png','Resolution',100)    
+%% Velocity profile along the z-direction (absolute value).
+figure('color', 'w'); set(gcf, 'Position', [100 100 600 450]);
+errorbar(Z, VVV_alongZ*1e6, VVV_alongZ_err*1e6,'LineStyle','none', 'Marker','*','MarkerSize', 5); hold on
+% % the simulation results: 
+data = readmatrix('F:\Simulation\202208_for_0811uPIVexp\Data\uPIVexp_H52\Ux_Z-direction.csv');
+Speed_Simu = data(1:10:end, 1); ZZ = data(1:10:end, 9);
+plot(ZZ*1e5, Speed_Simu*1e7, 'r--.', 'LineWidth', 2); hold on
+xlabel('$Z\ ({\mu}m)$','FontSize',22,'Interpreter', 'latex');
+ylabel('$U_x\ ({\mu}m/s)$','FontSize',22,'Interpreter', 'latex');
+% % title('Speed in the Middle Plane','FontSize',48);
+set(gca,'FontSize',20)
+legend('PIV','Simulation','location','best','FontSize',20)
+xlim([-0.5 52.5])
+% f=gcf;
+% exportgraphics(f,'D:\Dropbox\Research\All Plottings\General plots\20220811_PIV-Simulation_PAsArea_H52_Ux-Z.png','Resolution',100)
 
 
-%%% Velocity profile along the flow-direction (absolute value).
-% XXX_alongX = ave_field_phy.y;
-% figure('color', 'w'); set(gcf, 'Position', [100 100 800 600]);
-% % % the simulation results: 
-% data = readmatrix('D:\Dropbox\PROCESS remotely\Simulation\202208\uPIVexp_H52\Ux_X-direction.csv');
-% Speed_Simu = data(1:2:end, 1); XX = data(1:2:end, 7);
-% errorbar(XXX_alongX(1:140), VVV_alongX(1:140)*1e6, VVV_alongX_err(1:140)*1e6,'LineStyle','none', 'Marker','*','MarkerSize', 5); hold on
-% plot(XX*1e5, Speed_Simu*1e7, 'r--.', 'LineWidth', 2); hold on
-% xlabel('$X\ ({\mu}m)$','FontSize',22,'Interpreter', 'latex');
-% ylabel('$U_x\ ({\mu}m/s)$','FontSize',22,'Interpreter', 'latex');
-% % % title('Speed in the Middle Plane','FontSize',48);
-% set(gca,'FontSize',20)
-% legend('PIV','Simulation','location','best','FontSize',20)
-% xlim([-0.5 115])
-% % f=gcf;
-% % exportgraphics(f,'D:\Dropbox\Research\All Plottings\General plots\20220811_PIV-Simulation_PAsArea_H52_Ux-X.png','Resolution',100)   
+%%% Velocity profile along the z-direction (normalized).
+figure('color', 'w'); set(gcf, 'Position', [100 100 600 450]);
+errorbar(Z/52-0.02, VVV_alongZ*1e6/147, VVV_alongZ_err*1e6/147,'LineStyle','none', 'Marker','*','MarkerSize', 5); hold on
+% % the simulation results: 
+data = readmatrix('F:\Simulation\202208_for_0811uPIVexp\Data\uPIVexp_H52\Ux_Z-direction.csv');
+Speed_Simu = data(1:10:end, 1); ZZ = data(1:10:end, 9);
+plot(ZZ*1e5/52, Speed_Simu*1e7/157, 'r--.', 'LineWidth', 2); hold on
+xlabel('$Z/H$','FontSize',22,'Interpreter', 'latex');
+ylabel('$U_x/U_{peak}\ in\ blank\ area$','FontSize',22,'Interpreter', 'latex');
+% % title('Speed in the Middle Plane','FontSize',48);
+set(gca,'FontSize',20)
+legend('PIV','Simulation','location','best','FontSize',20)
+xlim([0 1])
+% f=gcf;
+% exportgraphics(f,'D:\Dropbox\Research\All Plottings\General plots\20220811_PIV-Simulation_PAsArea_H52_Ux-Z_normalized.png','Resolution',100)    
 
 
-%%% Velocity profile along the flow-direction (normalized).
-% XXX_alongX = ave_field_phy.y;
-% figure('color', 'w'); set(gcf, 'Position', [100 100 800 600]);
-% % % the simulation results: 
-% data = readmatrix('D:\Dropbox\PROCESS remotely\Simulation\202208\uPIVexp_H52\Ux_X-direction.csv');
-% Speed_Simu = data(1:2:end, 1); XX = data(1:2:end, 7);
-% errorbar(XXX_alongX(1:140), VVV_alongX(1:140)*1e6/147, VVV_alongX_err(1:140)*1e6/147,'LineStyle','none', 'Marker','*','MarkerSize', 5); hold on
-% plot(XX*1e5, Speed_Simu*1e7/157, 'r--.', 'LineWidth', 2); hold on
-% xlabel('$X\ ({\mu}m)$','FontSize',22,'Interpreter', 'latex');
-% ylabel('$U_x/U_{peak}\ in\ blank\ area$','FontSize',22,'Interpreter', 'latex');
-% % % title('Speed in the Middle Plane','FontSize',48);
-% set(gca,'FontSize',20)
-% legend('PIV','Simulation','location','best','FontSize',20)
-% xlim([-0.5 115])
-% % f=gcf;
-% % exportgraphics(f,'D:\Dropbox\Research\All Plottings\General plots\20220811_PIV-Simulation_PAsArea_H52_Ux-X_normalized.png','Resolution',100)  
+%% Velocity profile along the flow-direction (absolute value).
+XXX_alongX = ave_field_phy.y;
+figure('color', 'w'); set(gcf, 'Position', [100 100 800 600]);
+% % the simulation results: 
+data = readmatrix('F:\Simulation\202208_for_0811uPIVexp\Data\uPIVexp_H52\Ux_X-direction.csv');
+Speed_Simu = data(1:2:end, 1); XX = data(1:2:end, 7);
+errorbar(XXX_alongX(1:140), VVV_alongX(1:140)*1e6, VVV_alongX_err(1:140)*1e6,'LineStyle','none', 'Marker','*','MarkerSize', 5); hold on
+plot(XX*1e5, Speed_Simu*1e7, 'r--.', 'LineWidth', 2); hold on
+xlabel('$X\ ({\mu}m)$','FontSize',22,'Interpreter', 'latex');
+ylabel('$U_x\ ({\mu}m/s)$','FontSize',22,'Interpreter', 'latex');
+% % title('Speed in the Middle Plane','FontSize',48);
+set(gca,'FontSize',20)
+legend('PIV','Simulation','location','best','FontSize',20)
+xlim([-0.5 115])
+% f=gcf;
+% exportgraphics(f,'D:\Dropbox\Research\All Plottings\General plots\20220811_PIV-Simulation_PAsArea_H52_Ux-X.png','Resolution',100)   
+
+
+%% Velocity profile along the flow-direction (normalized).
+XXX_alongX = ave_field_phy.y;
+figure('color', 'w'); set(gcf, 'Position', [100 100 800 600]);
+% % the simulation results: 
+data = readmatrix('F:\Simulation\202208_for_0811uPIVexp\Data\uPIVexp_H52\Ux_X-direction.csv');
+Speed_Simu = data(1:2:end, 1); XX = data(1:2:end, 7);
+errorbar(XXX_alongX(1:140), VVV_alongX(1:140)*1e6/147, VVV_alongX_err(1:140)*1e6/147,'LineStyle','none', 'Marker','*','MarkerSize', 5); hold on
+plot(XX*1e5, Speed_Simu*1e7/157, 'r--.', 'LineWidth', 2); hold on
+xlabel('$X\ ({\mu}m)$','FontSize',22,'Interpreter', 'latex');
+ylabel('$U_x/U_{peak}\ in\ blank\ area$','FontSize',22,'Interpreter', 'latex');
+% % title('Speed in the Middle Plane','FontSize',48);
+set(gca,'FontSize',20)
+legend('PIV','Simulation','location','best','FontSize',20)
+xlim([-0.5 115])
+% f=gcf;
+% exportgraphics(f,'D:\Dropbox\Research\All Plottings\General plots\20220811_PIV-Simulation_PAsArea_H52_Ux-X_normalized.png','Resolution',100)  
 
 %% Blank area
 clear; close all; clc
 
-selpath = uigetdir('G:\PhD, PMMH, ESPCI\Experimental Data (EXTRACTED)\');
+selpath = uigetdir('F:\Processing & Results\PIV & PTV\20220811-uPIV\20220811_Channel2022-07-26_FlowAngle45deg_Characterization\Avg_Std\\');
 listing = dir(selpath);
 
 Names = {listing.name};  % the file/folder names
 blankArea = and(contains(Names,'Lens='), contains(Names,'BlankArea_')); % only select the Blank-area related files/folders.
-thefolder_ind = cell2mat({listing.isdir}); % only select the folders.
-PAsArea = and(blankArea, thefolder_ind); % the Blank-area folders.
-PAsArea_ind = find(PAsArea==1); % the indices of the Blank-area folders.
+PAsArea_ind = find(blankArea==1); % the indices of the Blank-area folders.
 
 magni = 0.067; % the magnification of the objective (um/pixel)
 
-layersNUM = sum(PAsArea);
+layersNUM = sum(blankArea);
 Z = zeros(1, layersNUM);  UUU_alongZ = zeros(1, layersNUM); VVV_alongZ = zeros(1, layersNUM);
 UUU_alongZ_err = zeros(1, layersNUM); VVV_alongZ_err = zeros(1, layersNUM); 
 for ii = 1:layersNUM
@@ -188,7 +184,7 @@ for ii = 1:layersNUM
 %     v = loadvec(fullfile(selpath,theONE,'\AddGeometricMask\PIV_MPd(2x48x48_75%ov)\*.vc7'));  % to calculate the ave_field
 %     ave_field = averf(v);
 %     [ave_field_AF, ave_field_STD, ave_field_RMS] = averf(v);
-    load(fullfile(selpath,theONE,'\AddGeometricMask\PIV_MPd(2x48x48_75%ov)\ave_field_STD_RMS.mat')); % load the ave_field
+    load(fullfile(selpath,theONE)); % load the ave_field
 %     save(fullfile(selpath,theONE,'\AddGeometricMask\PIV_MPd(2x48x48_75%ov)\ave_field_STD_RMS.mat'), "ave_field_AF", "ave_field_RMS", "ave_field_STD");
     ave_field_phy = ave_field_AF; % in physical units.
     ave_field_phy.vx = ave_field_AF.vx * magni / deltaT; % velocity in m/s.
@@ -228,64 +224,64 @@ for ii = 1:layersNUM
 %     ax = gca; ax.Title.Interpreter = 'latex';
 end
 
-%%% Velocity profile along the z-direction (absolute value).
-% figure('color', 'w'); set(gcf, 'Position', [100 100 600 450]);
-% errorbar(Z, VVV_alongZ*1e6,VVV_alongZ_err*1e6, 'LineStyle','none', 'Marker','*','MarkerSize', 5); hold on
-% % % % the simulation results: 
-% data = readmatrix('D:\Dropbox\PROCESS remotely\Simulation\202208\uPIVexp_H52\Ux_Z-direction_BlankArea.csv');
-% Speed_Simu = data(1:10:end, 1); ZZ = data(1:10:end, 9);
-% plot(ZZ*1e5, Speed_Simu*1e7, 'r--.', 'LineWidth', 2); hold on
-% % % % theory
-% a = 26;   % height/2 (um)
-% b = 200; % width/2 (um)
-% Q = 2 * 10^(6); %flow rate (nL! the number before *)
-% y = 0;
-% z = -a:(2*a/100):a;
-% Sigma1 = 0; Sigma2 = 0;
-% for i = 1:2:10^2
-%     Sigma1 = Sigma1 + (-1)^((i-1)/2)*(1-(cosh(i*pi*y/(2*a)))/(cosh(i*pi*b/(2*a))))*(cos(i*pi*z/(2*a)))/(i^3);
-%     Sigma2 = Sigma2 + (tanh(i*pi*b/(2*a)))/(i^5);
-% end
-% Foo = 1 / (1 - 192*a*Sigma2/(pi^5*b));
-% f = (12*Q)/((pi)^3*a*b)*Sigma1*Foo;
-% plot(z+26, f, 'LineWidth', 2, 'Color','y'); hold on
-% xlabel('$Z\ ({\mu}m)$','FontSize',22,'Interpreter', 'latex');
-% ylabel('$U_x\ ({\mu}m/s)$','FontSize',22,'Interpreter', 'latex');
-% set(gca,'FontSize',20)
-% xlim([-0.5 52.5])
-% legend('PIV','Simulation','Theory','location','best','FontSize',20)
-% % f=gcf;
-% % exportgraphics(f,'D:\Dropbox\Research\All Plottings\General plots\20220811_PIV-Simulation-Theory_BlankArea_H52_Ux-Z.png','Resolution',100)
+%% Velocity profile along the z-direction (absolute value).
+figure('color', 'w'); set(gcf, 'Position', [100 100 600 450]);
+errorbar(Z, VVV_alongZ*1e6,VVV_alongZ_err*1e6, 'LineStyle','none', 'Marker','*','MarkerSize', 5); hold on
+% % % the simulation results: 
+data = readmatrix('F:\Simulation\202208_for_0811uPIVexp\Data\uPIVexp_H52\Ux_Z-direction_BlankArea.csv');
+Speed_Simu = data(1:10:end, 1); ZZ = data(1:10:end, 9);
+plot(ZZ*1e5, Speed_Simu*1e7, 'r--.', 'LineWidth', 2); hold on
+% % % theory
+a = 26;   % height/2 (um)
+b = 200; % width/2 (um)
+Q = 2 * 10^(6); %flow rate (nL! the number before *)
+y = 0;
+z = -a:(2*a/100):a;
+Sigma1 = 0; Sigma2 = 0;
+for i = 1:2:10^2
+    Sigma1 = Sigma1 + (-1)^((i-1)/2)*(1-(cosh(i*pi*y/(2*a)))/(cosh(i*pi*b/(2*a))))*(cos(i*pi*z/(2*a)))/(i^3);
+    Sigma2 = Sigma2 + (tanh(i*pi*b/(2*a)))/(i^5);
+end
+Foo = 1 / (1 - 192*a*Sigma2/(pi^5*b));
+f = (12*Q)/((pi)^3*a*b)*Sigma1*Foo;
+plot(z+26, f, 'LineWidth', 2, 'Color','y'); hold on
+xlabel('$Z\ ({\mu}m)$','FontSize',22,'Interpreter', 'latex');
+ylabel('$U_x\ ({\mu}m/s)$','FontSize',22,'Interpreter', 'latex');
+set(gca,'FontSize',20)
+xlim([-0.5 52.5])
+legend('PIV','Simulation','Theory','location','best','FontSize',20)
+% f=gcf;
+% exportgraphics(f,'D:\Dropbox\Research\All Plottings\General plots\20220811_PIV-Simulation-Theory_BlankArea_H52_Ux-Z.png','Resolution',100)
 
 
-%%% Velocity profile along the spanwise-direction (absolute value).
-% figure('color', 'w'); set(gcf, 'Position', [100 100 600 450]);
-% UUU_alongY = UUU_alongY/counting; VVV_alongY = VVV_alongY/counting;
-% UUU_alongY_err = UUU_alongY_err/counting; VVV_alongY_err = VVV_alongY_err/counting;
-% XXX_alongY = ave_field_phy.x;
-% % errorbar(XXX_alongY, VVV_alongY*1e6, VVV_alongY_err*1e6,'LineStyle','none', 'Marker','*','MarkerSize', 5)
-% plot(XXX_alongY(1:end-2), VVV_alongY(1:end-2)*1e6,'LineStyle','none', 'Marker','*','MarkerSize', 5); hold on % (end - 2): remove the 0 value.
-% % % % theory
-% a = 200;   % height/2 (um)
-% b = 26; % width/2 (um)
-% Q = 2 * 10^(6); %flow rate (nL! the number before *)
-% y = 0;
-% z = -a:(2*a/100):a;
-% Sigma1 = 0; Sigma2 = 0;
-% for i = 1:2:10^2
-%     Sigma1 = Sigma1 + (-1)^((i-1)/2)*(1-(cosh(i*pi*y/(2*a)))/(cosh(i*pi*b/(2*a))))*(cos(i*pi*z/(2*a)))/(i^3);
-%     Sigma2 = Sigma2 + (tanh(i*pi*b/(2*a)))/(i^5);
-% end
-% Foo = 1 / (1 - 192*a*Sigma2/(pi^5*b));
-% f = (12*Q)/((pi)^3*a*b)*Sigma1*Foo;
-% plot(z+212.5, f, 'LineWidth', 2, 'Color','y'); hold on
-% xlabel('$Y\ ({\mu}m)$','FontSize',22,'Interpreter', 'latex');
-% ylabel('$U_x\ ({\mu}m/s)$','FontSize',22,'Interpreter', 'latex');
-% set(gca,'FontSize',20)
-% xlim([-0.5 420.5])
-% legend('PIV','Theory','location','best','FontSize',20)
-% % f=gcf;
-% % exportgraphics(f,'D:\Dropbox\Research\All Plottings\General plots\20220811_PIV-Theory_BlankArea_H52_Ux-Y.png','Resolution',100)
+%% Velocity profile along the spanwise-direction (absolute value).
+figure('color', 'w'); set(gcf, 'Position', [100 100 600 450]);
+UUU_alongY = UUU_alongY/counting; VVV_alongY = VVV_alongY/counting;
+UUU_alongY_err = UUU_alongY_err/counting; VVV_alongY_err = VVV_alongY_err/counting;
+XXX_alongY = ave_field_phy.x;
+% errorbar(XXX_alongY, VVV_alongY*1e6, VVV_alongY_err*1e6,'LineStyle','none', 'Marker','*','MarkerSize', 5)
+plot(XXX_alongY(1:end-2), VVV_alongY(1:end-2)*1e6,'LineStyle','none', 'Marker','*','MarkerSize', 5); hold on % (end - 2): remove the 0 value.
+% % % theory
+a = 200;   % height/2 (um)
+b = 26; % width/2 (um)
+Q = 2 * 10^(6); %flow rate (nL! the number before *)
+y = 0;
+z = -a:(2*a/100):a;
+Sigma1 = 0; Sigma2 = 0;
+for i = 1:2:10^2
+    Sigma1 = Sigma1 + (-1)^((i-1)/2)*(1-(cosh(i*pi*y/(2*a)))/(cosh(i*pi*b/(2*a))))*(cos(i*pi*z/(2*a)))/(i^3);
+    Sigma2 = Sigma2 + (tanh(i*pi*b/(2*a)))/(i^5);
+end
+Foo = 1 / (1 - 192*a*Sigma2/(pi^5*b));
+f = (12*Q)/((pi)^3*a*b)*Sigma1*Foo;
+plot(z+212.5, f, 'LineWidth', 2, 'Color','y'); hold on
+xlabel('$Y\ ({\mu}m)$','FontSize',22,'Interpreter', 'latex');
+ylabel('$U_x\ ({\mu}m/s)$','FontSize',22,'Interpreter', 'latex');
+set(gca,'FontSize',20)
+xlim([-0.5 420.5])
+legend('PIV','Theory','location','best','FontSize',20)
+% f=gcf;
+% exportgraphics(f,'D:\Dropbox\Research\All Plottings\General plots\20220811_PIV-Theory_BlankArea_H52_Ux-Y.png','Resolution',100)
 
 
 
