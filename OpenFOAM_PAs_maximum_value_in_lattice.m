1ca1e2ae13cd392411315231bf4203d4ff4fe152
Victor Lee <zhibo.li.1994@gmail.com>
2023-07-25 09:12:50 +0200

    Add: plot U-profiles and U_mag_max ratio. Change figure axis ticks properties.

--- OpenFOAM_PAs_maximum_value_in_lattice.m
+++ OpenFOAM_PAs_maximum_value_in_lattice.m
@@ -9,6 +9,15 @@ clear; close all; clc
 % Initial velocity
 U_0 = 1e-5;
 
+cmap_1 = cmocean('ice');
+cmap_2 = cmocean('solar');
+figure('color', 'w'); set(gcf, 'Position', [100 100 800 600]); % plot the v-profiles
+
+plot(nan, nan, '-', 'Color', 'k', 'LineWidth',1);
+hold on
+plot(nan, nan, ':', 'Color', 'k', 'LineWidth',1);
+hold on
+
 n = 1;
 for Array_angle = 0:5:45
 
@@ -65,17 +74,39 @@ for Array_angle = 0:5:45
     Pos_up{n} = yy(yy_start:yy_end, xx_start);
     n = n + 1;
 
-%     figure; 
-%     plot(xx(yy_start, xx_start:xx_end), U_mag(yy_start, xx_start:xx_end), 'r');
-%     hold on
-%     plot(yy(yy_start:yy_end, xx_start), U_mag(yy_start:yy_end, xx_start), 'g');
+    U_plot_1 = U_mag(yy_start, xx_start:xx_end);
+    plot(linspace(0, 1, length(U_plot_1)), U_plot_1, '-', 'Color', cmap_1((n+5)*15, :), 'LineWidth',2);
+    hold on
+    U_plot_2 = U_mag(yy_start:yy_end, xx_start);
+    plot(linspace(0, 1, length(U_plot_2)), U_plot_2, '--', 'Color', cmap_2((n+5)*15, :), 'LineWidth',2);
+    hold on
   
 end
 
-tilt_angle = 0:5:45;
-save('D:\Dropbox\GitHub\Postprocessing_PIV\Velocity_on_lattice_edge.mat', ...
-    "U_mag_up", "U_mag_left", "U_mag_max_up", "U_mag_max_left", "Pos_up", ...
-    "Pos_left", "tilt_angle")
+legend({'$|U|_{\rm AD}/|U_0|$', '$|U|_{\rm AB}/|U_0|$'},'FontSize', 24, ...
+    'Interpreter', 'latex', 'Box','off', 'Location','northwest' )
+
+set(gca,'Box', 'On','XGrid', 'On', 'YGrid', 'On', 'GridAlpha', 0.3, 'FontSize', 24,'TickLabelInterpreter','latex')
+
+ylim([0 5.0001])
+ylabel('$|U|/|U_0|$','FontSize', 24,'Interpreter', 'latex');
+xlabel('$\alpha_0\,(^\circ)$','FontSize', 24,'Interpreter', 'latex');
+
+hhh = gcf;
+set(hhh,'Units','Inches');
+pos = get(hhh,'Position');
+set(hhh,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)])
+print(hhh, '-dpdf',['D:\Dropbox\Research\My PhD thesis\Figures\5-flexible_fiber_array' ...
+    '\flow field\U_mag_profiles.pdf']);
+
+set(gcf,'renderer','Painters');
+print('-depsc2','-tiff','-r100','-vector',['D:\Dropbox\Research\My PhD thesis\' ...
+    'Figures\5-flexible_fiber_array\flow field\U_mag_profiles.eps']);
+
+% tilt_angle = 0:5:45;
+% save('D:\Dropbox\GitHub\Postprocessing_PIV\Velocity_on_lattice_edge.mat', ...
+%     "U_mag_up", "U_mag_left", "U_mag_max_up", "U_mag_max_left", "Pos_up", ...
+%     "Pos_left", "tilt_angle")
 
 
 
@@ -95,13 +126,13 @@ ylabel('$|U|_{\rm AB,\,max}/|U_0|$','FontSize', 24,'Interpreter', 'latex');
 yyaxis right
 plot(tilt_angle, U_mag_max_up, 'o','MarkerSize', 12,'MarkerEdgeColor','k', ...
                 'MarkerFaceColor', [207 67 62]/255);
-ylim([0 5.0035])
+ylim([-0.035 5.0035])
 ylabel('$|U|_{\rm AD,\,max}/|U_0|$','FontSize', 24,'Interpreter', 'latex');
 
 xlabel('$\alpha_0\,(^\circ)$','FontSize', 24,'Interpreter', 'latex');
 xlim([0 45.00001]); xticks([0:15:45]);
 
-set(gca,'Box', 'On','XGrid', 'On', 'YGrid', 'On', 'GridAlpha', 0.5, 'FontSize', 24)
+set(gca,'Box', 'On','XGrid', 'On', 'YGrid', 'On', 'GridAlpha', 0.5, 'FontSize', 24,'TickLabelInterpreter','latex')
 
 ax = gca;
 ax.YAxis(1).Color = [64 57 144]/255;
@@ -122,15 +153,33 @@ clear; close all; clc
 load('D:\Dropbox\GitHub\Postprocessing_PIV\Velocity_on_lattice_edge.mat');
 for ii = 1:10
 
-    integral_ratio(ii) = trapz(Pos_up{ii}, U_mag_up{ii}) / trapz(Pos_left{ii}, U_mag_left{ii});
+%     integral_ratio(ii) = trapz(Pos_up{ii}, U_mag_up{ii}) / trapz(Pos_left{ii}, U_mag_left{ii});
     max_Umag_ratio(ii) = U_mag_max_up(ii) / U_mag_max_left(ii);
 
 end
-figure; plot(integral_ratio);
+% figure; plot(integral_ratio);
 
-figure; plot(max_Umag_ratio)
-hold on; plot(tand(tilt_angle))
+figure('color', 'w'); set(gcf, 'Position', [100 100 800 600]);
+plot(tilt_angle, max_Umag_ratio, 'o','MarkerSize', 12,'MarkerEdgeColor','k', ...
+                'MarkerFaceColor', 'm')
+hold on; plot(0:45, tand(0:45), '--k','LineWidth',1)
 
+ylim([0 1.0035])
+ylabel('$|U|_{\rm AD,\,max}/|U|_{\rm AB,\,max}$','FontSize', 24,'Interpreter', 'latex');
+
+xlabel('$\alpha_0\,(^\circ)$','FontSize', 24,'Interpreter', 'latex');
+xlim([0 45.00001]); xticks([0:15:45]);
+
+legend({'', '$\tan\alpha_0$'},'FontSize', 24,'Interpreter', 'latex', 'Box','off', 'Location','northwest' )
+
+set(gca,'Box', 'On','XGrid', 'On', 'YGrid', 'On', 'GridAlpha', 0.3, 'FontSize', 24,'TickLabelInterpreter','latex')
+
+hhh = gcf;
+set(hhh,'Units','Inches');
+pos = get(hhh,'Position');
+set(hhh,'PaperPositionMode','Auto','PaperUnits','Inches','PaperSize',[pos(3), pos(4)])
+print(hhh, '-dpdf',['D:\Dropbox\Research\My PhD thesis\Figures\5-flexible_fiber_array' ...
+    '\flow field\U_mag_max_ratio.pdf']);
 
 
 %%
@@ -193,7 +242,7 @@ ylabel('$\sigma_{\rm max}$','FontSize', 24,'Interpreter', 'latex');
 xlabel('$\alpha_0\,(^\circ)$','FontSize', 24,'Interpreter', 'latex');
 xticks([0:15:45]);
 
-set(gca,'Box', 'On','XGrid', 'On', 'YGrid', 'On', 'GridAlpha', 0.5, 'FontSize', 24)
+set(gca,'Box', 'On','XGrid', 'On', 'YGrid', 'On', 'GridAlpha', 0.5, 'FontSize', 24,'TickLabelInterpreter','latex')
 
 hhh = gcf;
 set(hhh,'Units','Inches');
